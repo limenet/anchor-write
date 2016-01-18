43ba3cc279e9cfd92ee71bf2d0eeeddbd5fee572
Linus Metzler <linus.metzler@gmail.com>
2016-01-18 23:32:16 +0100

    whitespace fixes

--- 404.php
+++ 404.php
@@ -5,11 +5,11 @@
     <div class="content">
 
         <h2 class="h2">Page not found</h2>
-          
+
         <p>Unfortunately, the page <code>/<?php echo current_url(); ?></code> could not be found.</p>
-        
+
     </div>
-    
+
 </div>
 
 <?php theme_include('footer'); ?>
--- header.php
+++ header.php
@@ -1,25 +1,25 @@
 <!doctype html>
 <html>
   <head>
-  
+
     <meta charset="utf-8">
-    
+
     <meta http-equiv="X-UA-Compatible" content="IE=edge,chrome=1">
-    
+
     <title><?php echo page_title('Page can’t be found'); ?> - <?php echo site_name(); ?></title>
-    
+
     <meta name="description" content="<?php echo site_description(); ?>">
-    
+
     <meta name="viewport" content="width=device-width, initial-scale=1">
-    
+
     <!--[if lt IE 9]>
       <script src="//html5shiv.googlecode.com/svn/trunk/html5.js"></script>
     <![endif]-->
-    
+
     <link rel="stylesheet" href="<?php echo theme_url('/css/i.css'); ?>">
-    
+
     <link rel="shortcut icon" href="<?php echo theme_url('/css/favicon.png'); ?>">
-    
+
     <meta name="generator" content="Anchor CMS">
 
     <?php if(customised()): ?>
@@ -29,15 +29,15 @@
     	<!--  Custom Javascript -->
     		<script><?php echo article_js(); ?></script>
 	<?php endif; ?>
-    
+
   </head>
-  
+
   <body>
-  
+
     <header class="top">
-    
+
         <h1 class="h1 site-title no-margin site-logo mega center lh-1 padding-top"><a href="<?php echo base_url(); ?>"><?php echo site_name(); ?></a></h1>
-      
+
         <h2 class="sans-serif caps title-spacing tagline center"><?php echo site_description(); ?></h2>
-      
+
     </header>
