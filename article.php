24ff1457faf3e7421c76d34b75c8cd423ab45aa7
Linus Metzler <linus.metzler@gmail.com>
2016-01-18 23:23:18 +0100

    working on #1

--- article.php
+++ article.php
@@ -1,43 +1,66 @@
 <?php theme_include('header'); ?>
 
 <div class="content-wrapper bg-third padding-top">
-    
-    <div class="content">
-      
-        <h2 class="small-title sans-serif caps">Written <time datetime="<?php echo date(DATE_W3C, article_time()); ?>"><?php echo relative_time(article_time()); ?></time> by <?php echo article_author('real_name'); ?>. This article is my <?php echo numeral(article_id()); ?> oldest. It is <?php echo count_words(article_markdown()); ?> words long.</h2>
-      
-        <h2 class="h2"><?php echo article_title(); ?></h2>
-          
-        <p><?php echo article_markdown(); ?></p>
-        
-    </div>
-        
-    <?php if(comments_open()): ?>
-    
-    <hr>
-      
-    <div class="content">
-     
-    <div id="disqus_thread"></div>
-    <script type="text/javascript">
-        /* * * CONFIGURATION VARIABLES: EDIT BEFORE PASTING INTO YOUR WEBPAGE * * */
-        var disqus_shortname = 'athlk'; // required: replace example with your forum shortname
 
-        /* * * DON'T EDIT BELOW THIS LINE * * */
-        (function() {
-            var dsq = document.createElement('script'); dsq.type = 'text/javascript'; dsq.async = true;
-            dsq.src = '//' + disqus_shortname + '.disqus.com/embed.js';
-            (document.getElementsByTagName('head')[0] || document.getElementsByTagName('body')[0]).appendChild(dsq);
-        })();
-    </script>
-    <noscript>Please enable JavaScript to view the <a href="http://disqus.com/?ref_noscript">comments powered by Disqus.</a></noscript>
-    <a href="http://disqus.com" class="dsq-brlink">comments powered by <span class="logo-disqus">Disqus</span></a>
-    
-          
+    <div class="content">
+
+        <h2 class="small-title sans-serif caps">Written <time datetime="<?php echo date(DATE_W3C, article_time()); ?>"><?php echo relative_time(article_time()); ?></time> by <?php echo article_author('real_name'); ?>. This article is my <?php echo numeral(article_id()); ?> oldest. It is <?php echo count_words(article_markdown()); ?> words long.</h2>
+
+        <h2 class="h2"><?php echo article_title(); ?></h2>
+
+        <p><?php echo article_markdown(); ?></p>
+
     </div>
-    
+    <?php if(comments_open()): ?>
+    <hr>
+    <div class="content">
+        <h3>Comments</h3>
+    </div>
+
+    <div class="content">
+        <?php if(has_comments()): ?>
+        <ul class="commentlist">
+            <?php $i = 0; while(comments()): $i++; ?>
+            <li class="comment" id="comment-<?php echo comment_id(); ?>">
+                <div class="wrap">
+                    <h4><?php echo comment_name(); ?></h4>
+
+
+                    <blockquote class="content">
+                        <?php echo htmlspecialchars(comment_text()); ?>
+                        <br><br><cite>&ndash; <time><?php echo relative_time(comment_time()); ?></time></cite>
+                    </blockquote>
+
+                </div>
+            </li>
+            <?php endwhile; ?>
+        </ul>
+        <hr>
+        <?php endif; ?>
+
+        <form method="post" action="<?php echo comment_form_url(); ?>#comment">
+            <?php echo comment_form_notifications(); ?>
+
+            <p>
+                <?php echo comment_form_input_name('placeholder="Your name"'); ?>
+            </p>
+
+            <p>
+                <?php echo comment_form_input_email('placeholder="Your email (won’t be published)"'); ?>
+            </p>
+
+            <p>
+                <?php echo comment_form_input_text('placeholder="Your comment" rows=4'); ?>
+            </p>
+
+            <p class="submit">
+                <?php echo comment_form_button(); ?>
+            </p>
+        </form>
+
+    </div>
     <?php endif; ?>
-        
+
 </div>
 
 <?php theme_include('footer'); ?>
