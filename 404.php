c70f7b9f5f1ec1ca07a211b917145309ac28d078
Linus Metzler <linus.metzler@gmail.com>
2016-01-19 11:44:42 +0100

    tidying up

--- 404.php
+++ 404.php
@@ -1,15 +1,8 @@
 <?php theme_include('header'); ?>
-
 <div class="content-wrapper bg-third padding-top">
-
     <div class="content">
-
         <h2 class="h2">Page not found</h2>
-
         <p>Unfortunately, the page <code>/<?php echo current_url(); ?></code> could not be found.</p>
-
     </div>
-
 </div>
-
 <?php theme_include('footer'); ?>
--- footer.php
+++ footer.php
@@ -1,21 +1,18 @@
     <footer class="footer center">
         <nav id="main" role="navigation">
-			<ul>
-				<?php if(has_menu_items()):
-					while(menu_items()): ?>
-        				<li <?php echo (menu_active() ? 'class="active"' : ''); ?>>
-        					<a href="<?php echo menu_url(); ?>" title="<?php echo menu_title(); ?>">
-        						<?php echo menu_name(); ?>
-        					</a>
-        				</li>
-				    <?php endwhile;
-				endif; ?>
-			</ul>
-		</nav>
-
+            <ul>
+                <?php if(has_menu_items()):
+                    while(menu_items()): ?>
+                        <li <?php echo (menu_active() ? 'class="active"' : ''); ?>>
+                            <a href="<?php echo menu_url(); ?>" title="<?php echo menu_title(); ?>">
+                                <?php echo menu_name(); ?>
+                            </a>
+                        </li>
+                    <?php endwhile;
+                endif; ?>
+            </ul>
+        </nav>
         <p>&copy; <?php echo date('Y'); ?> / <a href="//anchorcms.com" target="_blank">Anchor</a></p>
-
     </footer>
-
     </body>
 </html>
