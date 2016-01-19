1250dd4412182a2cfe3999f0677754b206d67c7e
Linus Metzler <linus.metzler@gmail.com>
2016-01-19 11:31:34 +0100

    add nav to footer

--- footer.php
+++ footer.php
@@ -1,6 +1,19 @@
     <footer class="footer center">
+        <nav id="main" role="navigation">
+			<ul>
+				<?php if(has_menu_items()):
+					while(menu_items()): ?>
+        				<li <?php echo (menu_active() ? 'class="active"' : ''); ?>>
+        					<a href="<?php echo menu_url(); ?>" title="<?php echo menu_title(); ?>">
+        						<?php echo menu_name(); ?>
+        					</a>
+        				</li>
+				    <?php endwhile;
+				endif; ?>
+			</ul>
+		</nav>
 
-        &copy; <?php echo date('Y'); ?> / <a href="//anchorcms.com" target="_blank">Anchor</a>
+        <p></p>&copy; <?php echo date('Y'); ?> / <a href="//anchorcms.com" target="_blank">Anchor</a></p>
 
     </footer>
 
