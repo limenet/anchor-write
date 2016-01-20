3d906510bb858eb7a45c2963a5cef115aecb1f1b
Linus Metzler <linus.metzler@gmail.com>
2016-01-20 11:28:27 +0100

    markup

--- article.php
+++ article.php
@@ -1,8 +1,10 @@
 <?php theme_include('header'); ?>
 <section id="main">
-        <h2><?php echo article_title(); ?></h2>
-        <p class="small-title">Written <time title="<?php echo date('Y-m-d H:i:s', article_time());?>" datetime="<?php echo date(DATE_W3C, article_time()); ?>"><?php echo relative_time(article_time()); ?></time> by <?php echo article_author('real_name'); ?> in <a href="<?php echo article_category_url(); ?>"><?php echo article_category(); ?></a>.</p>
-        <p><?php echo article_markdown(); ?></p>
+        <article>
+            <h2><?php echo article_title(); ?></h2>
+            <p class="small-title">Written <time title="<?php echo date('Y-m-d H:i:s', article_time());?>" datetime="<?php echo date(DATE_W3C, article_time()); ?>"><?php echo relative_time(article_time()); ?></time> by <?php echo article_author('real_name'); ?> in <a href="<?php echo article_category_url(); ?>"><?php echo article_category(); ?></a>.</p>
+            <?php echo article_markdown(); ?>
+        </article>
     <?php if(comments_open()): ?>
     <hr class="spacing">
         <h3 id="comment">Comments</h3>
