d448a511269e47d50d5087b16e6534d2c8e72383
Linus Metzler <linus.metzler@gmail.com>
2016-01-20 11:26:22 +0100

    markup

--- posts.php
+++ posts.php
@@ -6,7 +6,7 @@
             <?php echo $i > 1 ? "<hr>" : ''; ?>
             <h2><a href="<?php echo article_url(); ?>"><?php echo article_title(); ?></a></h2>
             <p class="small-title">Written <time title="<?php echo date('Y-m-d H:i:s', article_time());?>" datetime="<?php echo date(DATE_W3C, article_time()); ?>"><?php echo relative_time(article_time()); ?></time> by <?php echo article_author('real_name'); ?> in <a href="<?php echo article_category_url(); ?>"><?php echo article_category(); ?></a>.</p>
-            <p><?php echo article_markdown();?></p>
+            <article><?php echo article_markdown();?></article>
         <?php endwhile; ?>
         <?php if(has_pagination()): ?>
             <hr class="spacing">
